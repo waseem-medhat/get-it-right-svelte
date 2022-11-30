6d307119ffb94fb884c2e0b301fccfbac1d96c0a
waseem-medhat <waseemmedhatg@gmail.com>
2022-11-30 12:20:05 +0200

    front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="container mt-16 pl-28 flex items-center space-x-16">
+	<img src="/get_it_right.png" alt="Get it Right" class="h-72" />
+	<span class="text-6xl font-light">+</span>
+	<img src="svelte.svg" alt="Get it Right" class="h-60" />
+</section>
+
+<section class="container mt-12 pl-36 flex items-center space-x-16">
+	<ol class="text-3xl list-decimal hover:text-orange-600">
+		<li><a href="/sol-lewitt">Sol Lewitt</a></li>
+	</ol>
+</section>
---
+++ src/routes/sol-lewitt/+page.svelte
@@ -0,0 +1,1 @@
+Sol Lewitt goes here
